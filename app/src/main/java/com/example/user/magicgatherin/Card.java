f51f575e5b08940be47d5f3b00cf73d17162e249
David <46406163y@iespoblenou.org>
2016-11-04 12:41:19 +0100

    Creacio del POJO

---
+++ app/src/main/java/com/example/user/magicgatherin/Card.java
@@ -0,0 +1,68 @@
+package com.example.user.magicgatherin;
+
+/**
+ * Created by user on 04/11/2016.
+ */
+import android.text.TextUtils;
+
+import java.util.ArrayList;
+
+public class Card {
+
+    private String name;
+    private String type;
+    private String rarity;
+    private String text;
+    private String power;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", text='" + text + '\'' +
+                ", power='" + power + '\'' +
+                '}';
+    }
+}
